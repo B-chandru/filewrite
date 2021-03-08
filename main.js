070a8fa71956ee39a4f24582d0ced98fe6e75a2d
Chandru <65664480+B-chandru@users.noreply.github.com>
2021-03-08 10:17:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const mic=document.getElementById("mic");
 
 
 mic.addEventListener("click",()=>{
-    const recog= new speechRecognition ();
+    const recog= new speechRecognition();
     recog.start();
     recog.onresult=(data)=>{
         document.getElementById("content").innerText = data.results[0][0].transcript;
